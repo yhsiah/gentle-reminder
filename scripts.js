7e69bcd3a1cf987cbecf4cabf841e798c9e4dd60
Han Siah <yhsiah90@gmail.com>
2018-10-29 10:20:12 +0000

    initial commit for overlay

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
-let scrolling_block;
-let wpadminbar = document.getElementById("wpadminbar");
+// let scrolling_block;
+// let wpadminbar = document.getElementById("wpadminbar");
 
 let textContent = "You're on PRODUCTION, be careful!"
-let blockHeight = 50;
+// let blockHeight = 50;
  
-scrolling_block = document.createElement( 'div' );
-scrolling_block.innerHTML = textContent;
-scrolling_block.id = "gr-scrolling-block";
-scrolling_block.className = "grStyles fixedElement";
+// scrolling_block = document.createElement( 'div' );
+// scrolling_block.innerHTML = textContent;
+// scrolling_block.id = "gr-scrolling-block";
+// scrolling_block.className = "grStyles fixedElement";
 
-document.body.classList.add('moveDown')
-document.documentElement.appendChild( scrolling_block );
-document.documentElement.insertAdjacentElement( 'afterbegin', scrolling_block );
+// document.body.classList.add('moveDown')
+// document.documentElement.appendChild( scrolling_block );
+// document.documentElement.insertAdjacentElement( 'afterbegin', scrolling_block );
 
 // commented out below is a WIP attempt to get this to work with WP-admin pages.
 
@@ -22,3 +22,16 @@ document.documentElement.insertAdjacentElement( 'afterbegin', scrolling_block );
 //         wpadminbar
 //       );
 // }
+
+let overlay
+
+overlay = document.createElement('div');
+overlay.id = "gr-overlay";
+overlay.className = "overlay";
+overlay.innerHTML = textContent;
+
+document.body.appendChild( overlay );
+
+// document.body.classList.add( 'overlay' );
+
+// document.documentElement.insertAdjacentElement( 'afterbegin', overlay );
